75dc3d5868204cbb88e0732abb4d1cdf63b28b7c
Josedetorre <josemdp@gmail.com>
2024-12-16 18:33:45 +0100

    flex - angulo

--- script/guirnalda.js
+++ script/guirnalda.js
@@ -5,33 +5,31 @@
         this.append (container);
 
         $(".guirnalda-container").each (function () {
-            
-            // Variables
-            var containerWidth = $(this).width();
-            var containerHeight = $(this).height();
-            var spacing = containerWidth / (numEsferas + 1); // Espaciado entre esferas
-            var yCenter = (containerHeight - 20) / 2; // Centrar verticalmente las esferas (resta 20px del tamaño)
-
             // Crear las esferas
             for (var i = 0; i < numEsferas; i++) {
-                // Calcular la posición X de cada esfera
-                var x = spacing * (i + 1) - 10; // -10 para centrar la esfera
+                
+                // Crear el contenedor de la esfera
+                var separador = $('<div class="esfera-container"></div>');
 
                 // Crear la esfera
                 var esfera = $('<div class="esfera"><div class="gorrito"></div></div>');
 
                 // Establecer la posición y un color aleatorio
+
+                var color = getRandomColor();
+
                 esfera.css({
-                    left: x + 'px',
-                    top: yCenter + 'px',
-                    backgroundColor: getRandomColor(), // Color aleatorio
-                    animationDelay: (((i + 1) % 3)*0.5) + 's' // Desfase dinámico
+                    backgroundColor: color, // Color aleatorio
+                    animationDelay: (((i + 1) % 3)*0.5) + 's', // Desfase dinámico
+                    rotate: getRandomInt(-30, 30) + 'deg',  // Rotación de la bombilla                
+                    boxShadow: '0 0 0.25rem ' + color,                    
                 });
 
                 if (window.console) console.log (i % 3);
 
                 // Añadir la esfera al contenedor
-                $(this).append(esfera);
+                $(separador).append(esfera);
+                $(this).append(separador);
             }
         });
 
@@ -42,12 +40,19 @@
 
     // Función para obtener un color aleatorio
     function getRandomColor() {
-        var letters = ['00FF00', 'FF0000', 'FFFF00'];
+        var bulbColor = ['00FF00', 'FF0000', 'E0E000'];
         var color = '#';
 
-        color += letters[document.guirnalda.ultimocolor ++ % 3];
+        color += bulbColor[document.guirnalda.ultimocolor ++ % 3];
         
         return color;
+    }
+
+    // Función para obtener un número aleatorio entre 2 valores
+    function getRandomInt(min, max) {
+        const minCeiled = Math.ceil(min);
+        const maxFloored = Math.floor(max);
+        return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
     }
 
 })(jQuery);
